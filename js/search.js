520cc832b7bed2febedb99841cf0d8d3e8dd8163
PRANTOSH <prantoshv@gmail.com>
2021-06-03 11:56:39 +0530

    added search

--- js/search.js
+++ js/search.js
@@ -1,1 +1,20 @@
-var search=document.getElementById()
+function searchUrl() {
+  var search = document.getElementById("search").value;
+  switch (search) {
+    case "login":
+    window.location.href='login.html'  
+        break;
+    case "projects":
+    window.location.href='index.html#projects';
+        break;
+    case "login":
+    window.location.href='';
+        break;
+    case "login":
+    window.location.href='';
+        break;
+    default:
+        window.alert("Search not found :(")
+      break;
+  }
+}
